bfbbf56c8359096af8d2eb4093cf98164fd6571d
Soren_nielsen <soren.nielsen599@gmail.com>
2023-04-26 22:05:54 -0600

    changed hw 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int pct = int.Parse(answer);
+
+        string grade = "";
+
+        if (pct >= 90)
+        {
+            grade = "A";
+        }
+        else if (pct >= 80)
+        {
+            grade = "B";
+        }
+        else if (pct >= 70)
+        {
+            grade = "C";
+        }
+        else if (pct >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {grade}");
+        
+        if (pct >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
