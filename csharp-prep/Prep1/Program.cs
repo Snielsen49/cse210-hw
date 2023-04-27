ed3705451a12fe1d259b7b14865bffa93e0e6af8
Soren_nielsen <soren.nielsen599@gmail.com>
2023-04-26 22:00:47 -0600

    added hm 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,16 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+    Console.Write("What is your first name? ");
+    string fst = Console.ReadLine();
+
+    Console.Write("What is your last name? ");
+    string lst = Console.ReadLine();
+
+    Console.WriteLine($"your name is {fst}, {fst} {lst}.");
     }
 }
